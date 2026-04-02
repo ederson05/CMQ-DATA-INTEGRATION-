041e6407f2545b5ad32e3c552fad5a52a0f43d51
Ederson05 <ederson@unicauca.edu.co>
2026-04-02 16:19:14 -0500

    prueba url

--- Proyecto/mi-proyecto/src/login.jsx
+++ Proyecto/mi-proyecto/src/login.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import './login.css'
 
+const API = 'https://cmq-backend.onrender.com/api'
+
 function Login() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
--- Proyecto/mi-proyecto/src/secretaria_principal.jsx
+++ Proyecto/mi-proyecto/src/secretaria_principal.jsx
@@ -7,7 +7,8 @@ import { FaStethoscope } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 import './secretaria_principal.css'
 
-const API = 'http://localhost:3001/api'
+//const API = 'http://localhost:3001/api'
+const API = 'https://cmq-backend.onrender.com/api'
 const hoy = new Date().toISOString().split('T')[0]
 
 const validarTelefono = (tel) => tel.replace(/\D/g, '').length === 10
